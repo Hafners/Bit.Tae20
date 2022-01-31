a4b6e1f80c4b83b767320103bdcd7189620edf0b
Stefan Hafner <stefan.hafner.development@gmail.com>
2022-01-31 10:31:52 +0100

    Add project files.

---
+++ Bit.Tae20.TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit.Tae20.TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Luca");
+        }
+    }
+}
